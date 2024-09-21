edec74f6f3af047870be2f336f905ed451c1e05b
Inocencio Cordeiro <cordeiroturorias@gmail.com>
2024-09-21 23:43:57 +0100

    new class

---
+++ src/main/java/org/example/ContaCorrente.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class ContaCorrente {
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+    public ContaCorrente() {  //construtor sugerido pela IDE
+        super();
+    }
+
+    public void imprimirExtrato() {
+        System.out.println("#### Extrato de Conta Corrente #####");
+        super.imprimirInfosComuns(); //metodo protegido para acesso de classes filhas
+    }
+
+}
